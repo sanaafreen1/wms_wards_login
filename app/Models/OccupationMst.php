d999da705d523dcc2705d7b7e9c2e8e9c00e2dab
sushmithachityala <sushmithachityala1@gmail.com>
2023-05-23 14:34:48 +0530

    add occupation
    add occupation

---
+++ app/Models/OccupationMst.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OccupationMst extends Model
+{
+    use HasFactory;
+    protected $table = 'occupation_mst';
+}
