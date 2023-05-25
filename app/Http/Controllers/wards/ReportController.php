02a3586bb3155a21c05766888b8d051a048b8b4d
sanaafreen1 <sanaafreen2572@gmail.com>
2023-05-25 15:27:28 +0530

    report
    report

--- app/Http/Controllers/wards/ReportController.php
+++ app/Http/Controllers/wards/ReportController.php
@@ -4,39 +4,26 @@ namespace App\Http\Controllers\wards;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\{FamilyMemberModel,OccupationMst,EducationMst};
+use App\Models\{FamilyMemberModel,OccupationMst,EducationMst,BasicDetailsModel};
 
 class ReportController extends Controller
 {
 
   public function wards_reports(Request $request ){
-dd($request->all());
-
-$query = FamilyMemberModel::query();
-
-if (request('search')) {
-    $query
-        ->where('owner_name', 'like', '%' . request('search') . '%')
-        ->orWhere('house_no', 'like', '%' . request('search') . '%')
-        ->orWhere('mobilenumber', 'like', '%' . request('search') . '%');
-}
-
-if ($request->has(['field', 'sortOrder']) && $request->field != null) {
-    $query->orderBy(request('field'), request('sortOrder'));
-}
-
-
-// return FamilyMemberModel::render('Users/Index', [
-//     'users' => fn() => $query->paginate(10),
-// ]);
 
 
         $occupation=OccupationMst::get();
-        $education= EducationMst::get();
+$education= EducationMst::get();
+$family=FamilyMemberModel::get();
 
-    return view('wards.reports',compact('education', 'occupation'));
+$details=BasicDetailsModel::get();
+    return view('wards.reports',compact('education', 'occupation','family','details'));
 }
 
-
+public function wards_family_report()
+{
+    $family=FamilyMemberModel::get();
+return view('wards.family-report',compact('family'));
+}
   }
 
