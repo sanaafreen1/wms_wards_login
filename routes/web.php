5b85ab4183f7df2eeeea32c28628d809dc526631
RathnakarN913 <rathnakarn09@gmail.com>
2023-05-31 17:43:46 +0530

    updated
    updated

--- routes/web.php
+++ routes/web.php
@@ -140,14 +140,7 @@ Route::get('wards_reports_edit', [ReportController::class, 'reports_edit'])->nam
 // Route::get('/wards-addmember/{id}', [ReportController::class, 'wards_add_members'])->name('wards_add_members');
 
 });
-// Varsha
 
-
-
-// rathnakar code
-// shruthi ias
-
-// Rathnakar code
 
 
 
