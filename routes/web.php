670a1e40725403b2459025f6ac95b43e83d253c6
akshayakonda <akshayakonda68@gmail.com>
2023-05-31 17:32:01 +0530

    testing
    testing

--- routes/web.php
+++ routes/web.php
@@ -140,3 +140,6 @@ Route::get('wards_reports_edit', [ReportController::class, 'reports_edit'])->nam
 // Route::get('/wards-addmember/{id}', [ReportController::class, 'wards_add_members'])->name('wards_add_members');
 
 });
+
+
+// rathnakar code
