6c38dda4f63c7343df5b11adaaca15d4c17267e8
sushmithachityala <sushmithachityala1@gmail.com>
2023-05-31 18:46:48 +0530

    service details controller
    service details controller

--- app/Http/Controllers/wards/EnterServiceDetailsController.php
+++ app/Http/Controllers/wards/EnterServiceDetailsController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers\wards;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\{CitizenServiceMst,CitizenSubServiceMst,DocumentMst,EnterServiceDetails};
+use App\Models\{CitizenServiceMst,CitizenSubServiceMst,DocumentMst,EnterServiceDetails,Enter_service_documentModel};
 use Illuminate\Support\Facades\Auth;
-
+use DB;
 
 class EnterServiceDetailsController extends Controller
-{ 
+{
    public function wards_enter_service()
    {
 
@@ -35,6 +35,7 @@ class EnterServiceDetailsController extends Controller
         'service' => 'required',
         'subservice' => 'required',
         'document' => 'required|array',
+        'service_status'=>'required',
     ]);
     $basicdetails_id=session()->get('basic_details_id');
     $house_ownerdetails_id=session()->get('house_ownerdetails_id');
@@ -43,24 +44,34 @@ class EnterServiceDetailsController extends Controller
       $cnt = count($request->document);
      $service= $request->service;
      $subservice = $request->subservice;
-      for($i = 0; $i < $cnt; $i++)
-      {
-        $document = $request->document[$i];
-        EnterServiceDetails::updateOrCreate([
+     $service_status=$request->service_status;
+
+
+        $details = EnterServiceDetails::updateOrCreate([
     'basic_details_id' =>$basicdetails_id,
       'house_owner_id'=>$house_ownerdetails_id,
             'service_id' => $service,
             'sub_service_id' => $subservice,
-            'document_id' => $document
+
+            'service_status'=>$service_status,
           ],
         [ 'basic_details_id' =>$basicdetails_id,
         'house_owner_id'=>$house_ownerdetails_id,
             'service_id' => $service,
             'sub_service_id' => $subservice,
-            'document_id' => $document,
+
+            'service_status'=>$service_status,
             'created_by' => Auth::id(),
         ]);
-      }
+
+        for($i = 0; $i < $cnt; $i++)
+        {
+            $document = $request->document[$i];
+            Enter_service_documentModel::insert([
+                'enter_service_id' => $details->id,
+                'document_id' => $document,
+            ]);
+        }
 
       return response()->json(['status' => 'success']);
 
@@ -72,47 +83,61 @@ class EnterServiceDetailsController extends Controller
 public function edit($id)
 {
     $service = CitizenServiceMst::get();
-  
+
     $document=DocumentMst::get();
     $update= EnterServiceDetails::where('basic_details_id','=',$id)->get();
+    $insert_document = Enter_service_documentModel::where('enter_service_id',$update[0]->id)->get();
+    // dd($insert_document);
+
 
     $serv_id = $update[0]->service_id;
     $subservice =CitizenSubServiceMst::where('service_id',$serv_id)->get();
-    
-    return view('wards.enter_service_edit',compact('update','service','document','subservice','serv_id'));
+
+    return view('wards.enter_service_edit',compact('update','service','document','subservice','serv_id','insert_document'));
 }
 
-public function update( Request $request){
+ public function update(Request $request){
 
-    $id = $request->id;
-
-    $basicdetails_id=session()->get('basic_details_id');
-    $house_ownerdetails_id=session()->get('house_ownerdetails_id');
+    // $basicdetails_id=session()->get('basic_details_id');
+    // $house_ownerdetails_id=session()->get('house_ownerdetails_id');
 
 
       $cnt = count($request->document);
      $service= $request->service;
      $subservice = $request->subservice;
-      for($i = 0; $i < $cnt; $i++)
-      {
-        $document = $request->document[$i];
-        EnterServiceDetails::where('id',$id)->update([
-    'basic_details_id' =>$basicdetails_id,
-      'house_owner_id'=>$house_ownerdetails_id,
-            'service_id' => $service,
-            'sub_service_id' => $subservice,
-            'document_id' => $document
-          ],
-        [ 'basic_details_id' =>$basicdetails_id,
-        'house_owner_id'=>$house_ownerdetails_id,
-            'service_id' => $service,
-            'sub_service_id' => $subservice,
-            'document_id' => $document,
-            'created_by' => Auth::id(),
-        ]);
-      }
+     $service_status=$request->service_status;
 
-      return response()->json(['status' => 'success']);
+
+     $id=$request->id;
+
+    $details = EnterServiceDetails::where('id',$id)->update(
+                [
+                    // 'basic_details_id' =>$basicdetails_id,
+                    // 'house_owner_id'=>$house_ownerdetails_id,
+                    'service_id' => $service,
+                    'sub_service_id' => $subservice,
+                    'service_status'=>$service_status,
+                    'created_by' => Auth::id(),
+                ]
+                );
+
+            // dd($details);
+
+            $del = Enter_service_documentModel::where('enter_service_id',$id)->delete();
+                    // dd($del);
+            for($i = 0; $i < $cnt; $i++)
+            {
+                $document = $request->document[$i];
+                Enter_service_documentModel::insert([
+                    'enter_service_id' => $id,
+                    'document_id' => $document,
+                ]);
+            }
+
+          return response()->json(['status' => 'success']);
+
+
+ }
 
 
 
@@ -120,4 +145,4 @@ public function update( Request $request){
 }
 
 
-}
+
