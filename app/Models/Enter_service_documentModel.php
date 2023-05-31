57e1ccb6eaf4005645135a199c539747a0760ad4
sushmithachityala <sushmithachityala1@gmail.com>
2023-05-31 18:47:46 +0530

    service details document model
    service details document model

---
+++ app/Models/Enter_service_documentModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Enter_service_documentModel extends Model
+{
+    use HasFactory;
+    protected $table='enter_service_doc_map';
+    protected $guarded=[];
+}
